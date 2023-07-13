077575e7403d18c64cc31a9b7ce8132023f09e79
rafa133169 <rafaelmonje1331@gmail.com>
2023-07-13 12:38:05 -0500

    estilos en css

---
+++ src/footer1.js
@@ -0,0 +1,79 @@
+import logo from './logo.svg';
+import './estilos.css';
+
+function footer1() {
+  return (
+    <div classname="footer1">
+
+
+
+
+			<form class="row g-3">
+
+				<div class="footer_correo">
+					<label for="inputcorreo" class="visually-hidden"></label>
+					<input type="email" placeholder="       Correo electronico" id="footer_correo">
+
+				</div>
+				<div >
+					<button class="footer_correo_boton" type="submit" class="btn btn-primary mb-3"><h3>Registrarse</h3></button>
+
+				</div>
+			</form>
+
+
+
+
+			<img style="width: 157px; height: 88px; left: 88px; top: 048px; position: absolute; box-shadow: 0px 4px 10px rgba(88.01, 176.97, 205.06, 0.93) inset"
+				src="imagen/image 115.png" />
+
+			<div class="footer_registrate_text">Registrate y comienza a comprar</div>
+
+
+
+			<img style="width: 62.42px; height: 62.42px; left: 111.80px; top: 377.35px; position: absolute; transform: rotate(-16.43deg); transform-origin: 0 0"
+				src="imagen/image 105.png" />
+			<img style="width: 84px; height: 84px; left: 1143px; top: 354.76px; position: absolute; transform: rotate(-16.43deg); transform-origin: 0 0"
+				src="imagen/image 105.png" />
+			<img style="width: 84px; height: 84px; left: 1336.60px; top: 346px; position: absolute; transform: rotate(25.07deg); transform-origin: 0 0"
+				src="imagen/image 105.png" />
+			<img style="width: 62.69px; height: 62.69px; left: 1316.52px; top: 244.86px; position: absolute; transform: rotate(8.30deg); transform-origin: 0 0"
+				src="imagen/image 105.png" />
+			<img style="width: 84px; height: 84px; left: 254.60px; top: 331px; position: absolute; transform: rotate(25.07deg); transform-origin: 0 0"
+				src="imagen/image 105.png" />
+			<!--inicia lo de en medio -->
+			<img style="width: 63.91px; height: 63.91px; left: 442.08px; top: 371px; position: absolute; transform: rotate(25.07deg); transform-origin: 0 0"
+				src="imagen/image 105.png" />
+			<img style="width: 63.91px; height: 63.91px; left: 875.55px; top: 380px; position: absolute; transform: rotate(3.19deg); transform-origin: 0 0"
+				src="imagen/image 105.png" />
+			<img style="width: 63.91px; height: 63.91px; left: 999px; top: 371px; position: absolute; transform: rotate(-17.08deg); transform-origin: 0 0"
+				src="imagen/image 105.png" />
+			<img style="width: 63.91px; height: 63.91px; left: 735px; top: 371px; position: absolute; transform: rotate(-17.08deg); transform-origin: 0 0"
+				src="imagen/image 105.png" />
+			<img class="paletapayaso" style="width: 63.91px; height: 63.91px; left: 577px; top: 395px; position: absolute; transform: rotate(-17.08deg); transform-origin: 0 0"
+				src="imagen/image 105.png" />
+			<!-- termina lo de en medio -->
+			<img class="paletapayaso" style="width: 84px; height: 84px; left: 42px; top: 274.21px; position: absolute; transform: rotate(-0.83deg); transform-origin: 0 0"
+				src="imagen/image 105.png" />
+			<img style="width: 48.60px; height: 48.60px; left: 189px; top: 283.70px; position: absolute; transform: rotate(-0.83deg); transform-origin: 0 0"
+				src="imagen/image 105.png" />
+			<img style="width: 48.60px; height: 48.60px; left: 1263px; top: 315.70px; position: absolute; transform: rotate(-0.83deg); transform-origin: 0 0"
+				src="imagen/image 105.png" />
+			<img style="width: 257px; height: 63px; left: 1110px; top: 048px; position: absolute"
+				src="imagen/image 129.png" />
+
+
+
+
+
+
+
+
+
+
+
+		</div>
+  );
+}
+
+export default footer1;
