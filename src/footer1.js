95e90dc6adc07848a069aa1e084f02e1c523e6ed
rafa133169 <rafaelmonje1331@gmail.com>
2023-07-13 13:13:51 -0500

    corrigiendo errores

--- src/footer1.js
+++ src/footer1.js
@@ -8,15 +8,15 @@ function footer1() {
 
 
 
-			<form class="row g-3">
+			<form classname="row g-3">
 
-				<div class="footer_correo">
+				<div classname="footer_correo">
 					<label for="inputcorreo" class="visually-hidden"></label>
-					<input type="email" placeholder="       Correo electronico" id="footer_correo">
+					<input type="email" placeholder="       Correo electronico" id="footer_correo" />
 
 				</div>
 				<div >
-					<button class="footer_correo_boton" type="submit" class="btn btn-primary mb-3"><h3>Registrarse</h3></button>
+					<button classname="footer_correo_boton" type="submit" class="btn btn-primary mb-3"><h3>Registrarse</h3></button>
 
 				</div>
 			</form>
@@ -27,7 +27,7 @@ function footer1() {
 			<img style="width: 157px; height: 88px; left: 88px; top: 048px; position: absolute; box-shadow: 0px 4px 10px rgba(88.01, 176.97, 205.06, 0.93) inset"
 				src="imagen/image 115.png" />
 
-			<div class="footer_registrate_text">Registrate y comienza a comprar</div>
+			<div classname="footer_registrate_text">Registrate y comienza a comprar</div>
 
 
 
@@ -41,7 +41,7 @@ function footer1() {
 				src="imagen/image 105.png" />
 			<img style="width: 84px; height: 84px; left: 254.60px; top: 331px; position: absolute; transform: rotate(25.07deg); transform-origin: 0 0"
 				src="imagen/image 105.png" />
-			<!--inicia lo de en medio -->
+			{/* inicia lo de en medio  */}
 			<img style="width: 63.91px; height: 63.91px; left: 442.08px; top: 371px; position: absolute; transform: rotate(25.07deg); transform-origin: 0 0"
 				src="imagen/image 105.png" />
 			<img style="width: 63.91px; height: 63.91px; left: 875.55px; top: 380px; position: absolute; transform: rotate(3.19deg); transform-origin: 0 0"
@@ -52,7 +52,7 @@ function footer1() {
 				src="imagen/image 105.png" />
 			<img class="paletapayaso" style="width: 63.91px; height: 63.91px; left: 577px; top: 395px; position: absolute; transform: rotate(-17.08deg); transform-origin: 0 0"
 				src="imagen/image 105.png" />
-			<!-- termina lo de en medio -->
+			 {/* termina lo de en medio  */}
 			<img class="paletapayaso" style="width: 84px; height: 84px; left: 42px; top: 274.21px; position: absolute; transform: rotate(-0.83deg); transform-origin: 0 0"
 				src="imagen/image 105.png" />
 			<img style="width: 48.60px; height: 48.60px; left: 189px; top: 283.70px; position: absolute; transform: rotate(-0.83deg); transform-origin: 0 0"
